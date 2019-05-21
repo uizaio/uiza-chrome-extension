7c6518d35989ccd4f4fc8f784d99d84f83bd06dc
trinvh <trinvh.com@gmail.com>
2019-05-21 10:31:13 +0700

    bypassed content-security-policy on some pages such as facebook, youtube...

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,1 +1,15 @@
 console.log('background !')
+chrome.webRequest.onHeadersReceived.addListener(details => {
+    for (var i = 0; i < details.responseHeaders.length; i++) {
+        var header = details.responseHeaders[i];
+        if (['x-content-security-policy', 'content-security-policy', 'x-webkit-csp'].indexOf(header.name.toLowerCase()) > -1) {
+            // details.responseHeaders.splice(i, 1);
+            console.log({...header});
+            header.value = "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';";
+        }
+    };
+
+    return {
+        responseHeaders: details.responseHeaders
+    };
+}, {urls: ["*://*/*"]}, ['blocking', 'responseHeaders']);
